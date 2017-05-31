1c1398b2c996c004a7b069511bbec202b411f0bc
wuxue <xuewu@epiclouds.net>
2017-05-31 19:58:06 +0800

    Update 实现开发环境多入口文件html webpack plugin配置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -11,7 +11,7 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
-module.exports = merge(baseWebpackConfig, {
+var devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
@@ -24,12 +24,33 @@ module.exports = merge(baseWebpackConfig, {
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    // https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }),
     new FriendlyErrorsPlugin()
   ]
 })
+
+// https://github.com/ampedandwired/html-webpack-plugin
+/**
+ * 每个配置对应于一个页面，有几个需要写几个
+ * filename: webpack编译指定文件，由html-webpack-plugin储存为html文件到输出目录。默认文件名为index.html
+ * template: 指定入口的html文件路径
+ * chunks: Limit the chunks being used
+ * inject: When passing true or 'body' all javascript resources will be
+   placed at the bottom of the body element
+ */
+var filename = ''
+config.build.entries.forEach(function(fname) {
+  if (fname === 'home') {
+    filename = 'index.html'
+  } else {
+    filename = fname + '.html'
+  }
+
+  devWebpackConfig.plugins.push(new HtmlWebpackPlugin({
+    'filename': filename,
+    'template': './src/pages/'+ fname +'/index.html',
+    'chunks': [fname],
+    'inject': true
+  }))
+})
+
+module.exports = devWebpackConfig
--- config/index.js
+++ config/index.js
@@ -19,7 +19,9 @@ module.exports = {
     View the bundle analyzer report after build finishes:
     `npm run build --report`
     Set to `true` or `false` to always turn it on or off*/
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
+    // 多入口文件名列表：每个页面对应一个同名文件夹
+    entries: ['home', 'about']
   },
   dev: {
     env: require('./dev.env'),
